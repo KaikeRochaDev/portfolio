679ce9f69b82c06f0d0f1dbc09150ad9eeab4de6
KaikeRochaDev <dev.kaikerocha@gmail.com>
2023-07-09 21:46:13 -0300

    Header component CSS and responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/eu.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Kaike Rocha</h1>
         <h5 className="text-light">Desenvolvedor Full Stack</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="eu" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub, FaInstagram} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/kaikerochadev/" target='_blank'></a>
-        <a href="https://github.com/kaikerochadev" target='_blank'></a>
-        <a href="https://www.instagram.com/kaikerochadev/" target='_blank'></a>
+        <a href="https://www.linkedin.com/in/kaikerochadev/" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/kaikerochadev" target='_blank'><FaGithub/></a>
+        <a href="https://www.instagram.com/kaikerochadev/" target='_blank'><FaInstagram/></a>
     </div>
   )
 }
