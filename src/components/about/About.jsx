875de30946f192ccdb533e53e3e7b84f5dcbf71f
KaikeRochaDev <dev.kaikerocha@gmail.com>
2023-07-10 22:28:24 -0300

    About Component JSX

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from "../../assets/me-about.jpg"
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Venha me conhecer</h5>
+      <h2>Sobre mim</h2>
+
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experiência</h5>
+              <small>3+ Anos de trabalho</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clientes</h5>
+              <small>200+ Mundialmente</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projetos</h5>
+              <small>80+ Completos</small>
+            </article>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae velit quis cupiditate quod corporis quasi, maiores placeat fuga quia consequatur odio dicta, doloribus provident voluptatum, hic quaerat molestias perferendis praesentium.</p>
+          <a href="#contact" className='btn btn-primary'>Vamos Conversar</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
