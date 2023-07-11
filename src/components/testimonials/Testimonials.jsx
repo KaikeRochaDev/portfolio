fd341644645122c397f28ff8d306930e25be890f
KaikeRochaDev <dev.kaikerocha@gmail.com>
2023-07-11 19:22:37 -0300

    Generating testimonials through map()

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,6 +4,34 @@ import AVTR1 from '../../assets/avatar1.jpg'
 import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi quidem nobis omnis beatae incidunt  accusamus quasi, aut reiciendis eum dolore eaque optio nemo. Modi porro facere atque non.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Shata Wale",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi quidem nobis omnis beatae incidunt quibusdam laudantium accusamus quasi, aut reiciendis eum dolore eaque optio nemo.  non.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum, consectetur adipisicing elit. Modi quidem nobis omnis beatae incidunt quibusdam laudantium accusamus quasi, aut reiciendis eum dolore eaque optio nemo. Modi porro facere atque non.",
+  },
+  {
+    avatar: AVTR4,
+    name: "Zofia Nagan",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi quidem nobis omnis beatae incidunt quibusdam laudantium accusamus quasi, aut reiciendis eum dolore eaque optio nemo.",
+  },
+];
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
@@ -11,47 +39,21 @@ const Testimonials = () => {
       <h2>Depoimentos</h2>
 
       <div className="container testimonials__container">
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar 1" />
-          </div>
-          <h5 className='client__name'>Ernest Achiever</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga quia, ullam optio, laudantium architecto, facilis voluptates iusto repudiandae esse ex quibusdam quod
-          </small>
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR2} alt="Avatar 2" />
-          </div>
-          <h5 className='client__name'>Michael Jhonson</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga quia, ullam optio, laudantium architecto, facilis voluptates iusto repudiandae esse ex quibusdam quod
-          </small>
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR3} alt="Avatar 3" />
-          </div>
-
-          <h5 className='client__name'>Logan Smith</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga quia, ullam optio, laudantium architecto, facilis voluptates iusto repudiandae esse ex quibusdam quod
-          </small>
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR4} alt="Avatar 4" />
-          </div>
-
-          <h5 className='client__name'>Zofia Nagan</h5>
-          <small className='client__review'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga quia, ullam optio, laudantium architecto, facilis voluptates iusto repudiandae esse ex quibusdam quod
-          </small>
-        </article>
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <article key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar}/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>
+                  {review}
+                </small>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
