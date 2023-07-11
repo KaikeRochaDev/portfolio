3509ae888e7683bd88bffa301527d41de551d245
KaikeRochaDev <dev.kaikerocha@gmail.com>
2023-07-11 15:21:44 -0300

    Portfolio Component JSX

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfólio</section>
+    <section id='portfolio'>
+      <h5>Meus Trabalhos Recentes</h5>
+      <h2>Portfólio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Isso é um título</h3>
+          <a href="https://github.com/kaikerochadev" target='_blank' className='btn'>GitHub</a>
+          <a href="https://github.com/kaikerochadev" target="_blank" className='btn btn-primary'>Ver site</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Isso é um título</h3>
+          <a href="https://github.com/kaikerochadev" target='_blank' className='btn'>GitHub</a>
+          <a href="https://github.com/kaikerochadev" target="_blank" className='btn btn-primary'>Ver site</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>Isso é um título</h3>
+          <a href="https://github.com/kaikerochadev" target='_blank' className='btn'>GitHub</a>
+          <a href="https://github.com/kaikerochadev" target="_blank" className='btn btn-primary'>Ver site</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>Isso é um título</h3>
+          <a href="https://github.com/kaikerochadev" target='_blank' className='btn'>GitHub</a>
+          <a href="https://github.com/kaikerochadev" target="_blank" className='btn btn-primary'>Ver site</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>Isso é um título</h3>
+          <a href="https://github.com/kaikerochadev" target='_blank' className='btn'>GitHub</a>
+          <a href="https://github.com/kaikerochadev" target="_blank" className='btn btn-primary'>Ver site</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>Isso é um título</h3>
+          <a href="https://github.com/kaikerochadev" target='_blank' className='btn'>GitHub</a>
+          <a href="https://github.com/kaikerochadev" target="_blank" className='btn btn-primary'>Ver site</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
